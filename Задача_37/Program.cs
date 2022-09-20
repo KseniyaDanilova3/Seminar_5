fe3d2a95829d98857e4f8cf606c2290f39404630
Kseniya <pashkina.ks@mail.ru>
2022-09-21 01:06:31 +0700

    Задача_37

---
+++ Задача_37/Program.cs
@@ -0,0 +1,40 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 9
+// [6 7 3 6] -> 36 21
+
+const int Size = 11; // решение с нечетным количеством элементов в массиве
+
+int[] array = CreateArray(Size, -10, 11);
+Console.WriteLine("Было:");
+PrintArray(array);
+Console.WriteLine("Стало:");
+PrintArray(MultiplyPair(array));
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++ )
+    {
+        Console.Write ($"{arr[i]} "); 
+    }
+    Console.WriteLine();
+}
+
+int[] CreateArray(int size, int smallest, int biggest)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(smallest,biggest+1);
+    }
+    return numbers;
+}
+
+int[] MultiplyPair(int[] arr)
+{
+    int[] multipliedVector = new int[(arr.Length + 1) / 2];
+    for (int i = 0; i < (arr.Length + 1) / 2; i++)
+    {
+        multipliedVector[i] = arr[i] * arr[arr.Length - i - 1];
+    }
+    return multipliedVector;
+}
